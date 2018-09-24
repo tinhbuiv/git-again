8ffd79128b7b4858ce4685b481454b79a4b9085e
tinhbuiv <tinhbvth1809016@fpt.edu.vn>
2018-09-24 18:20:21 +0700

    Hello

---
+++ cmake-build-debug/CMakeFiles/git_again.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_again.dir/main.c.obj"
+  "git_again.pdb"
+  "git_again.exe"
+  "git_again.exe.manifest"
+  "libgit_again.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_again.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/git_again.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for git_again.
+# This may be replaced when dependencies are built.
